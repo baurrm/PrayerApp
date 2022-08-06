01cb9d90bc799267fd16563eec180c5b98b23185
Bauyrzhan Marat <baurm12@gmail.com>
2022-08-06 23:07:38 +0200

    First commit

---
+++ Namaz/Controller/TimeViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Namaz
+//
+//  Created by Bauyrzhan Marat on 01.08.2022.
+//
+
+import UIKit
+
+class TimeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+//    var timeManager = TimeManager()
+    
+    @IBOutlet weak var cityName: UILabel!
+    
+    //Access users location
+    @IBAction func locationButton(_ sender: UIButton) {
+
+    }
+    
+    //Acess to the QiblaViewController
+    @IBAction func compassButton(_ sender: UIButton) {
+        
+    }
+    
+    
+    
+    
+}
+
